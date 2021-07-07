18d8b160359ea5c5ebbdafa345e6f8d4803594e9
dgumenyuk <gumenyuk98@gmail.com>
2021-07-07 11:50:42 -0400

    Delete RQ3 directory

--- RQ3/full_model/MyDuplicates.py
+++
@@ -1,6 +0,0 @@
-from pymoo.model.duplicate import ElementwiseDuplicateElimination
-
-class MyDuplicateElimination(ElementwiseDuplicateElimination):
-
-    def is_equal(self, a, b):
-        return a.X[0].states == b.X[0].states
